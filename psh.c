60145db6ea50e63d762a3a65e5980b6c4c423362
NickOmweri <nick.omweri@gmail.com>
2023-06-22 17:59:26 -0700

    Changes

---
+++ psh.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * f_psh - It adds a node to the stack.
+ * @counter: s the line number.
+ * @head: Is the stack head.
+ * Return: Nothing.
+ */
+
+void f_psh(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
+}
