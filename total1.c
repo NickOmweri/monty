5575f1bff71363007ace6c0bdc268fbb5f6543b2
NickOmweri <nick.omweri@gmail.com>
2023-06-25 15:38:31 -0700

    Fixed_typo

--- total1.c
+++ total1.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
- * f_tot2 - It adds node to the head stack.
+ * f_tot1 - It adds node to the head stack.
  * @n: Is the new value.
  * @head: Is the head of the stack.
  * Return: Nothing.
  */
 
-void f_tot2(stack_t **head, int n)
+void f_tot1(stack_t **head, int n)
 {
 	stack_t *new_node, *aux;
 
